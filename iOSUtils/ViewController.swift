1973260e0366d3b4856459bdabe48033cc01a872
halseywu <wsh940808@163.com>
2018-12-13 17:37:51 +0800

    新增权限请求工具（todo）

--- iOSUtils/ViewController.swift
+++ iOSUtils/ViewController.swift
@@ -8,9 +8,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 
     @IBAction func clickBtnTest(_ sender: UIButton) {
-        
+        PermissionUtils.init().jumpToSetting(controller: self, title: "fdsa", message: "fdsafdsa", defaultAction: "去设置", isJumpToSetting: true, cancelAction: "取消")
     }
     
 }
--- iOSUtils/utils/PermissionUtils.swift
+++ iOSUtils/utils/PermissionUtils.swift
@@ -2,8 +2,91 @@
 //  PermissionUtils.swift
 //  iOSUtils
 //
-//  Created by HalseyW-15 on 2018/11/21.
+//  Created by Wushhhhhh on 2017/8/25.
 //  Copyright © 2018年 wushhhhhh. All rights reserved.
 //
 
 import Foundation
+import AVFoundation
+import UIKit
+
+class PermissionUtils {
+
+    func configurePermission(permission: Permisson) {
+        switch permission {
+        case .camera:
+            requestPermissionForCamera()
+        case .mic:
+            requestPermissionForMic()
+        case .photo:
+            requestPermissionForPhoto()
+        case .location:
+            requestPermissionForLocation()
+        case .notification:
+            requestPermissionForNotification()
+        }
+    }
+    
+    func requestPermissionForCamera() {
+        AVCaptureDevice.requestAccess(for: .video) { (isAuthorized) in
+            if !isAuthorized {
+                self.denied()
+            }
+        }
+    }
+    
+    func denied() {
+        print("PermissionUtils -> Permission has been denied")
+    }
+    
+    func requestPermissionForMic() {
+        
+    }
+    
+    func requestPermissionForPhoto() {
+        
+    }
+    
+    func requestPermissionForLocation() {
+        
+    }
+    
+    func requestPermissionForNotification() {
+        
+    }
+    
+    
+    func jumpToSetting(controller: UIViewController, title: String, message: String, defaultAction: String?, isJumpToSetting: Bool, cancelAction: String?) {
+        NotificationCenter.default.addObserver(self, selector: #selector(back), name: UIApplication.didBecomeActiveNotification, object: nil)
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if let defaultActionTitle = defaultAction {
+            alertController.addAction(UIAlertAction(title: defaultActionTitle, style: .default, handler: { (_) in
+                if isJumpToSetting {
+                    if let URL = URL(string: UIApplication.openSettingsURLString) {
+                        UIApplication.shared.open(URL, options: [:], completionHandler: nil)
+                    }
+                }
+            }))
+        }
+        if let cancelActionTitle = cancelAction {
+            alertController.addAction(UIAlertAction(title: cancelActionTitle, style: .cancel, handler: { (_) in
+                
+            }))
+        }
+        controller.present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc func back() {
+        print("back")
+    }
+}
+
+enum Permisson {
+    case camera
+    case mic
+    case photo
+    case location
+    case notification
+}
+
